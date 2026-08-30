bf5e531129b86985ea3ee2464b6dcdbc5bfe3381
Harnoor Singh <harnoorsingh1150@gmail.com>
2026-08-30 13:31:15 +0530

    Time: 1 ms (98.68%), Space: 42.6 MB (42.88%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2)
+        return x;
+        int l=1;
+        int h=x/2;
+        int ans=0;
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            long sq=(long)mid*mid;
+            if(sq==x)
+            {
+            return mid;
+            }
+            else if(sq<x)
+            {
+            ans=mid;
+            l=mid+1;
+            }
+            else
+            {
+                h=mid-1;
+            }
+
+        }
+
+
+      return ans;  
+    }
+    
+}
