3ad3eeffd35ce6f752e1bfc62b505fac6e955a83
Harnoor Singh <harnoorsingh1150@gmail.com>
2026-09-22 10:32:21 +0530

    Time: 449 ms (60.27%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select a.firstname , a.lastname , b.city , b.state
 from person a
-join address b on a.personid = b.personid;
+left join address b on a.personid = b.personid;
